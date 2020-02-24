30dc11b0c92ac636602e910f7130b9bca60e27ce
SeanWelshBrown <seanwelshbrown@gmail.com>
2020-02-24 16:21:23 -0500

    Done.

--- src/Components/TVShowList.js
+++ src/Components/TVShowList.js
@@ -1,18 +1,64 @@
 import React, { Component } from 'react';
 import {Grid} from 'semantic-ui-react';
 
+import TVShow from '../Components/TVShow'
+
 class TVShowList extends Component {
 
   mapAllShows = () => {
-    if (!!props.searchTerm){
-      props.shows.map((s) => {
-        if (s.name.toLowerCase().includes(props.searchTerm)){
-          (<TVShow show={s} key={s.id} selectShow={props.selectShow}/> )
+    let filteredShows;
+    let showsToShow;
+    if (this.props.filterByRating !== "") {
+      filteredShows = this.props.shows.filter( show => {
+        let roundedShowRating = Math.round(show.rating.average)
+        let foundShow;
+        if (roundedShowRating === parseInt(this.props.filterByRating, 10)) {
+          foundShow = show
         }
+        return foundShow
       })
+      if (!!this.props.searchTerm) {
+        showsToShow = filteredShows.map((s) => {
+          if (s.name.toLowerCase().includes(this.props.searchTerm)) {
+            return <TVShow show={s} key={s.id} selectShow={this.props.selectShow}/>
+          } else {
+            return null
+          }
+        })
+      } else {
+        showsToShow = filteredShows.map( (s)=> <TVShow show={s} key={s.id} selectShow={this.props.selectShow}/>)
+      }
+    } else {
+      if (!!this.props.searchTerm) {
+        showsToShow = this.props.shows.map((s) => {
+          if (s.name.toLowerCase().includes(this.props.searchTerm)) {
+            return <TVShow show={s} key={s.id} selectShow={this.props.selectShow}/>
+          } else {
+            return null
+          }
+        })
+      } else {
+        showsToShow = this.props.shows.map( (s)=> <TVShow show={s} key={s.id} selectShow={this.props.selectShow}/>)
+      }
     }
-    return props.shows.map( (s)=> <TVShow show={s} key={s.id} selectShow={props.selectShow}/>)
+    return showsToShow
   }
+
+  // mapAllShows = () => {
+  //   let showsToShow;
+  //   if (!!this.props.searchTerm) {
+  //     showsToShow = this.props.shows.map((s) => {
+  //       if (s.name.toLowerCase().includes(this.props.searchTerm)) {
+  //         return <TVShow show={s} key={s.id} selectShow={this.props.selectShow}/>
+  //       } else {
+  //         return null
+  //       }
+  //     })
+  //   } else {
+  //     showsToShow = this.props.shows.map( (s)=> <TVShow show={s} key={s.id} selectShow={this.props.selectShow}/>)
+  //   }
+  //   return showsToShow
+  // }
 
   render() {
     return (
